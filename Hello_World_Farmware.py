8f14c6bee9269c6ef8d9bd3fbc2b798d3c0d8b71
Erick Fiestas Sorogastúa <efiestass@upao.edu.pe>
2018-06-04 13:22:11 -0500

    Update Hello_World_Farmware.py

--- Hello_World_Farmware.py
+++ Hello_World_Farmware.py
@@ -4,6 +4,7 @@ from CeleryPy import log
 import sys
 
 if __name__ == "__main__":
-    FARMWARE_NAME = ((__file__.split(os.sep))[len(__file__.split(os.sep)) - 3]).replace('-master', '')
-    log('Start... hello world!', message_type='info', title=FARMWARE_NAME)
-    sys.setdefaultencoding('utf8')
+    """FARMWARE_NAME = ((__file__.split(os.sep))[len(__file__.split(os.sep)) - 3]).replace('-master', '')"""
+    """log('Start... hello world!', message_type='info', title=FARMWARE_NAME)"""
+    log('Helllooo fukiiinnn worlllddd')
+    """sys.setdefaultencoding('utf8')"""
