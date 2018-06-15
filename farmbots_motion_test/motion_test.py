02f7531ffdd0f9db8eb9833b2591b3d654c51a78
Erick Fiestas Sorogastúa <efiestass@upao.edu.pe>
2018-06-15 11:30:08 -0500

    Update motion_test.py

--- farmbots_motion_test/motion_test.py
+++ farmbots_motion_test/motion_test.py
@@ -4,20 +4,5 @@ from CeleryPy import log
 import sys
 
 if __name__ == "__main__":
-    FARMWARE_NAME = ((__file__.split(os.sep))[len(__file__.split(os.sep)) - 3]).replace('-master', '')
-    log('Starting the fabulous farmware of ours!', message_type='info', title=FARMWARE_NAME)
-    reload(sys)
-    sys.setdefaultencoding('utf8')
-    try:
-        farmware = MyFarmware(FARMWARE_NAME)
-    except Exception as e:
-        log(e, message_type='error', title=FARMWARE_NAME + " : init")
-        raise Exception(e)
-    else:
-        try:
-            farmware.run()   """Initiate the process"""
-        except Exception as e:
-            log(e, message_type='error', title=FARMWARE_NAME + " : run")
-            raise Exception(e)
-    log('Ending our farmware', message_type='info', title=FARMWARE_NAME)
+    log('Motion please come true!!!!!')
 
