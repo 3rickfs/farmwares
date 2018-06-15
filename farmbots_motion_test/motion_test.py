6b3fad0d43a4cfca1e69abe9c2afd87a5d4bdd92
Erick Fiestas Sorogastúa <efiestass@upao.edu.pe>
2018-06-15 12:04:56 -0500

    Update motion_test.py

--- farmbots_motion_test/motion_test.py
+++ farmbots_motion_test/motion_test.py
@@ -6,3 +6,18 @@ import sys
 if __name__ == "__main__":
     FARMWARE_NAME = "Motion_test"
     log('Come on, man, just move yourself on', message_type='info', title=FARMWARE_NAME)
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    try:
+        farmware = MyFarmware(FARMWARE_NAME)
+    except Exception as e:
+        log(e, message_type='error', title=FARMWARE_NAME + " : init")
+        raise Exception(e)
+    else:
+        try:
+            farmware.run()   """Initiate the process"""
+        except Exception as e:
+            log(e, message_type='error', title=FARMWARE_NAME + " : run")
+            raise Exception(e)
+    log('Ending our farmware', message_type='info', title=FARMWARE_NAME)
+ 
