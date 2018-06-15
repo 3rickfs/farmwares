cd5b8d1f442ae1e7680e364896605143c09ae4cd
Erick Fiestas Sorogastúa <efiestass@upao.edu.pe>
2018-06-15 11:01:32 -0500

    Update Hello_World_Farmware.py

--- farmbot_test_1/Hello_World_Farmware.py
+++ farmbot_test_1/Hello_World_Farmware.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os
 from CeleryPy import log
+import sys
 
 if __name__ == "__main__":
-    FARMWARE_NAME = ((__file__.split(os.sep))[len(__file__.split(os.sep)) - 3]).replace('-master', '')
-    log('Starting the fabulous farmware of ours!', message_type='info', title=FARMWARE_NAME)
+    log('Helllooo fukiiinnn worlllddd')
 
 
