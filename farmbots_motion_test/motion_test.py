7f01bc41de7ef2ce60d94fcb28c7a28ed27ff61f
Erick Fiestas Sorogastúa <efiestass@upao.edu.pe>
2018-06-15 11:33:35 -0500

    Update motion_test.py

--- farmbots_motion_test/motion_test.py
+++ farmbots_motion_test/motion_test.py
@@ -4,5 +4,5 @@ from CeleryPy import log
 import sys
 
 if __name__ == "__main__":
-    log('Motion please come true!!!!!')
-
+    FARMWARE_NAME = ((__file__.split(os.sep))[len(__file__.split(os.sep)) - 3]).replace('-master', '')
+    log(FARMWARE_NAME)
